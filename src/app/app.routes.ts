39bedc5d23ee98cdf67d42a15c064cdb9020241a
serious03black <146652203+Serious03black@users.noreply.github.com>
2025-06-17 19:38:47 +0530

    login signup added page ,routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,32 @@ import { LandingComponent } from './HomePage/landing/landing.component';
 import { CallerComponent } from './VideoCall/caller/caller.component';
 import { ReciverComponent } from './VideoCall/reciver/reciver.component';
 import { SelectSymptomsComponent } from './CreateVideoCall/select-symptoms/select-symptoms.component';
+import { LoginComponent } from './Auth/login/login.component';
+import { RegisterComponent } from './Auth/register/register.component';
 
 export const routes: Routes = [
-    {
-        path:'', component:LandingComponent
-    },
-    {
-        path:'caller', component:CallerComponent
-    },
-    {
-        path:'reciver', component:ReciverComponent
-    },
-    {
-        path:'selectSymptoms', component: SelectSymptomsComponent
-    }
+  {
+    path: '',
+    component: LandingComponent,
+  },
+  {
+    path: 'caller',
+    component: CallerComponent,
+  },
+  {
+    path: 'reciver',
+    component: ReciverComponent,
+  },
+  {
+    path: 'selectSymptoms',
+    component: SelectSymptomsComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
 ];
